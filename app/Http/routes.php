f6fb807c0563e57909d58fb562e7c5ae5f3d6cea
mengkongly <mengkong2009@yahoo.com>
2018-11-02 18:41:11 -0500

    Initialize with migration

---
+++ app/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+use App\User;
+use App\Model\Role;
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}/role',function($id){
+    return User::findOrFail($id)->roles;
+
+});
